aa4b551cc57a92e511d718a95be21c6034309c08
Runn1dDefault <runniddefault@gmail.com>
2022-04-20 15:54:09 +0600

    complete project v1

---
+++ scrapers/tests.py
@@ -0,0 +1,40 @@
+import unittest
+from urllib.parse import urlparse
+
+from scrapers import StyleInForm
+from utils import read_data
+
+
+class TestStyleInForm(unittest.TestCase):
+    def setUp(self) -> None:
+        self.scraper = StyleInForm()
+
+    def test_all_products_urls(self):
+        products_urls = self.scraper.all_products_urls()
+
+        self.assertIsInstance(products_urls, list, msg='all_products_urls method not returned list')
+        self.assertNotEqual(len(products_urls), 0, msg='all_products_urls method return empty list')
+
+        for url in products_urls:
+            self.assertIsNotNone(urlparse(url).netloc, msg='not url in list')
+            self.assertIsNotNone(urlparse(url).scheme, msg='not url in list')
+
+    def test_login(self):
+        cookies = self.scraper.login()
+        self.assertIsInstance(cookies, dict, msg='method login must returned dict')
+        self.assertNotEqual(len(cookies.keys()), 0, msg='request to login not returned cookies dict')
+
+    def test_parse_all_products(self):
+        self.scraper.parse_all_products(5)
+        data = read_data()
+        self.assertEqual(data['products_count'], 0, msg='wrong qty in file')
+        self.assertIsNone(data['products'], msg='not added new data in file')
+
+        for p_data in data['products']:
+            self.assertIsInstance(p_data, dict)
+
+            self.assertIn('product_sku', p_data.keys())
+
+            if p_data.get('inventory_qty') and p_data['inventory_qty'] == 0:
+                self.assertIn('inventory_text', p_data.keys())
+
